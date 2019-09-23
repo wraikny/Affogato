b82cd6f7ccd0b5c796aa39548cab753513ae5c6b
wraikny <writtenknown@gmail.com>
2019-09-23 11:23:06 +0900

    support

--- src/Affogato/Support.fs
+++ src/Affogato/Support.fs
@@ -1,5 +1,5 @@
 [<AutoOpen>]
-module internal Affogato.Support
+module Affogato.Support
 
 let inline zero< ^a when ^a: (static member Zero: ^a) > = LanguagePrimitives.GenericZero< ^a >
 let inline one< ^a when ^a: (static member One: ^a) > = LanguagePrimitives.GenericOne< ^a >
@@ -7,10 +7,20 @@ let inline one< ^a when ^a: (static member One: ^a) > = LanguagePrimitives.Gener
 let inline map (f: 'a -> 'b) (x: ^``Functor<'a>``) : '``Functor<'b>`` =
   (^``Functor<'a>``: (static member Map: _*_->_) x, f)
 
+let inline (|>>) (x: ^``Functor<'a>``) (f: 'a -> 'b) : '``Functor<'b>`` = map f x
+
 let inline map2 (f: 'a -> 'b -> 'c) (x: ^``Functor<'a>``) (y: ^``Functor<'b>``) : '``Functor<'c>`` =
   ( (^``Functor<'a>`` or ^``Functor<'b>``): (static member Map2: _*_*_->_) x, y, f)
 
 let inline pure' x: ^a = (^a: (static member Return: _->_) x)
 
-let inline toSeq (x: ^a) =
-  (^a : (static member ToSeq:_->seq<_>) x)
+let inline toSeq (x: ^``Foldable<'a>``) =
+  (^``Foldable<'a>`` : (static member ToSeq:_->seq<_>) x)
+
+let inline toArray (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.toArray
+let inline toList (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.toList
+let inline toMap (x: ^``Foldable<'a>``) = x |> toSeq |> Map.ofSeq
+let inline toSet (x: ^``Foldable<'a>``) = x |> toSeq |> Set.ofSeq
+
+let inline exist f (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.exists f
+let inline forall f (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.forall f
