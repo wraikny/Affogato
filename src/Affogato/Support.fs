f3ec2006c84677bb4867e2079a3c94eb5ec818c4
wraikny <writtenknown@gmail.com>
2019-09-25 20:49:52 +0900

    add support

--- src/Affogato/Support.fs
+++ src/Affogato/Support.fs
@@ -19,13 +19,20 @@ let inline map2 (f: ^a -> ^b -> ^c) (x: ^``Functor<'a>``) (y: ^``Functor<'b>``) 
 
 let inline pure' x: ^a = (^a: (static member Return: _->_) x)
 
-let inline toSeq (x: ^``Foldable<'a>``) =
-  (^``Foldable<'a>`` : (static member ToSeq:_->seq<_>) x)
+let inline isNan x = System.Double.IsNaN (float x)
+let inline isInfinity x = System.Double.IsInfinity (float x)
 
-let inline toArray (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.toArray
-let inline toList (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.toList
-let inline toMap (x: ^``Foldable<'a>``) = x |> toSeq |> Map.ofSeq
-let inline toSet (x: ^``Foldable<'a>``) = x |> toSeq |> Set.ofSeq
+let inline isInvalid x = isNan x || isInfinity x
 
-let inline exist f (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.exists f
-let inline forall f (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.forall f
+[<AutoOpen>]
+module Foldable =
+  let inline toSeq (x: ^``Foldable<'a>``) =
+    (^``Foldable<'a>`` : (static member ToSeq:_->seq<_>) x)
+
+  let inline toArray (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.toArray
+  let inline toList (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.toList
+  let inline toMap (x: ^``Foldable<'a>``) = x |> toSeq |> Map.ofSeq
+  let inline toSet (x: ^``Foldable<'a>``) = x |> toSeq |> Set.ofSeq
+
+  let inline exist f (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.exists f
+  let inline forall f (x: ^``Foldable<'a>``) = x |> toSeq |> Seq.forall f
