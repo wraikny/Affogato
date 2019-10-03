82a611bade484b04f6b83350828ab38dfc79f99a
wraikny <writtenknown@gmail.com>
2019-10-03 14:22:26 +0900

    build script fixed

--- build.cmd
+++ build.cmd
@@ -1,3 +1,2 @@
-if not exist .fake/fake.exe dotnet tool install --tool-path ".fake" fake-cli --add-source https://api.nuget.org/v3/index.json
 .paket/paket.exe restore
-.fake\fake.exe run build.fsx %*
+.\fake.cmd run build.fsx %*
--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-if [ ! -f .fake/fake ]; then
-    dotnet tool install --tool-path ".fake" fake-cli --add-source https://api.nuget.org/v3/index.json
-fi
 mono .paket/paket.exe restore
-.fake/fake run build.fsx $@
+./fake.sh run build.fsx $@
