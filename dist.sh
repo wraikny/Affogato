50a7acd884a7bca7014cfefed49b17321c1a156f
wraikny <writtenknown@gmail.com>
2019-10-03 16:30:17 +0900

    fix dist

--- dist.sh
+++ dist.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-rm -r ./dist
-mkdir ./dist
+
+if [ -e ./dist]; then
+    rm ./dist/*
+else
+    mkdir ./dist
+fi
+
 cp ./output/Release/netstandard2.0/*.dll ./dist/.
 cp ./output/Release/netstandard2.0/*.pdb ./dist/.
 zip ./dist/Affogato.zip -r ./dist
